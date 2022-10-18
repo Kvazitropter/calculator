297f9f112945d2000ff420edf30bcb9e423fa00c
Kvazitropter <kachulinatanya@yandex.ru>
2022-10-18 18:49:08 +0300

    improve flexible fontsize and hide outut overflow

--- scripts/setOutputFontSize.js
+++ scripts/setOutputFontSize.js
@@ -1,7 +1,11 @@
 const setFontSize = () => {
   const outputHeight = outputWindow.getBoundingClientRect().height;
-  if (outputHeight > 75) {
+  if (outputHeight >= 75) {
     outputWindow.style.fontSize = `${outputHeight / 3}px`;
+  } else if (outputHeight >= 50) {
+    outputWindow.style.fontSize = `${outputHeight / 2}px`;
+  } else {
+    outputWindow.style.fontSize = `${outputHeight}px`;
   }
 };
 
